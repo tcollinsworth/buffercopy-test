6ff2bc2bc06204b5954d38404d9375a40034a871
troy <troycollinsworth@gmail.com>
2020-04-12 22:10:51 -0400

    added java files

---
+++ buffercopy-java/BufferCopy.java
@@ -0,0 +1,61 @@
+package buffercopy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class BufferCopy {
+
+	int metadataLength = 10;
+	int scoreLength = 4;
+	int metadataAndScoreLength = metadataLength + scoreLength;
+
+	int assets = 200000; // 1000000;
+	// 200K 11 ms
+	// 1M 50 ms
+
+	byte[] contentMetadataBuffer = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a };
+	byte[] scoreBuffer = { 0x0b, 0x0c, 0x0d, 0x0e };
+
+	List<byte[]> contentMetadataList = new ArrayList<>(assets);
+	List<byte[]> scoreList = new ArrayList<>(assets);
+
+	int[] indexes = new int[assets];
+
+	public static void main(String[] args) {
+		BufferCopy bc = new BufferCopy();
+		bc.thisMain();
+	}
+
+	private void thisMain() {
+		Random rnd = new Random();
+
+		for (int i = 0; i < assets; i++) {
+			contentMetadataList.add(i, contentMetadataBuffer.clone());
+			scoreList.add(i, scoreBuffer.clone());
+			indexes[i] = rnd.nextInt(assets);
+		}
+
+		for (int x = 0; x < 100000; x++) {
+			long startNs = System.nanoTime();
+
+			byte[][] responeList = new byte[assets][metadataAndScoreLength];
+			for (int i = 0; i < assets; i++) {
+				System.arraycopy(contentMetadataList.get(i), 0, responeList[i], 0, metadataLength);
+				System.arraycopy(scoreList.get(i), 0, responeList[i], metadataLength, scoreLength);
+				// System.out.println(byteArrayToHex(responeList[i]));
+			}
+
+			long endNs = System.nanoTime();
+			System.out.println(String.format("%d %d us", x, (endNs - startNs) / 1000));
+		}
+	}
+
+	public static String byteArrayToHex(byte[] ba) {
+		StringBuilder sb = new StringBuilder(ba.length * 2);
+		for (byte b : ba) {
+			sb.append(String.format("%02x", b));
+		}
+		return sb.toString();
+	}
+}
